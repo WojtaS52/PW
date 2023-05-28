ede890facb641d98014eca389df0ebda53291b0b
MatRyb <matti-2002@wp.pl>
2023-05-29 00:47:55 +0200

    Added model tests

---
+++ project/ModelTests/IValidatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using Model.API;
+
+namespace ModelTests
+{
+    // model tests by MR & WS
+    [TestClass]
+    public class IValidatorTest
+    {
+        [TestMethod]
+        public void BallCountValidatorTest()
+        {
+            const int min = 1;
+            const int max = 20;
+
+            InterfaceValidator<int> validator = new ValidatorKulek(min, max);
+
+            Assert.IsTrue(validator.IsValid(max - 1));
+            Assert.IsTrue(validator.IsValid(min + 1));
+
+            Assert.IsFalse(validator.IsValid(max + 1));
+            Assert.IsFalse(validator.IsValid(min - 1));
+        }
+    }
+}
---
+++ project/ModelTests/ModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.API;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void CreateModelApiTest()
+        {
+            ModelAbstractApi modelApi = ModelAbstractApi.StworzModelApi();
+            Assert.IsNotNull(modelApi);
+        }
+    }
+}
